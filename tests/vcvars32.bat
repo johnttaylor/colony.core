519f239e6d4d648a596bd7f9a987eef5f413e360
John Taylor <john.t.taylor@gmail.com>
2015-02-22 21:13:48 -0600

    update vcvars32.bat for publishing

--- tests/vcvars32.bat
+++ tests/vcvars32.bat
@@ -1,26 +1,27 @@
 
 @call :GetVSCommonToolsDir
-@if "%VS110COMNTOOLS%"=="" goto error_no_VS110COMNTOOLSDIR
+@if "%VS120COMNTOOLS%"=="" goto error_no_VS120COMNTOOLSDIR
 
-@call "%VS110COMNTOOLS%VCVarsQueryRegistry.bat" 32bit No64bit
+@call "%VS120COMNTOOLS%VCVarsQueryRegistry.bat" 32bit No64bit
 
 @if "%VSINSTALLDIR%"=="" goto error_no_VSINSTALLDIR
+@if "%VCINSTALLDIR%"=="" goto error_no_VCINSTALLDIR
 @if "%FrameworkDir32%"=="" goto error_no_FrameworkDIR32
 @if "%FrameworkVersion32%"=="" goto error_no_FrameworkVer32
-@if "%Framework35Version%"=="" goto error_no_Framework35Version
+@if "%Framework40Version%"=="" goto error_no_Framework40Version
 
 @set FrameworkDir=%FrameworkDir32%
 @set FrameworkVersion=%FrameworkVersion32%
 
-@if not "%WindowsSdkDir_old%" == "" (
-	@set "PATH=%WindowsSdkDir_old%bin\NETFX 4.0 Tools;%WindowsSdkDir_35%;%PATH%"
+@if not "%WindowsSDK_ExecutablePath_x86%" == "" (
+	@set "PATH=%WindowsSDK_ExecutablePath_x86%;%PATH%"
 )
 
 @if not "%WindowsSdkDir%" == "" (
 	@set "PATH=%WindowsSdkDir%bin\x86;%PATH%"
 	@set "INCLUDE=%WindowsSdkDir%include\shared;%WindowsSdkDir%include\um;%WindowsSdkDir%include\winrt;%INCLUDE%"
-	@set "LIB=%WindowsSdkDir%lib\win8\um\x86;%LIB%"
-	@set "LIBPATH=%WindowsSdkDir%References\CommonConfiguration\Neutral;%ExtensionSDKDir%\Microsoft.VCLibs\11.0\References\CommonConfiguration\neutral;%LIBPATH%"
+	@set "LIB=%WindowsSdkDir%lib\winv6.3\um\x86;%LIB%"
+	@set "LIBPATH=%WindowsSdkDir%References\CommonConfiguration\Neutral;%ExtensionSDKDir%\Microsoft.VCLibs\12.0\References\CommonConfiguration\neutral;%LIBPATH%"
 )
 
 @rem
@@ -36,11 +37,16 @@
 @if exist "%ProgramFiles%\HTML Help Workshop" set PATH=%ProgramFiles%\HTML Help Workshop;%PATH%
 @if exist "%ProgramFiles(x86)%\HTML Help Workshop" set PATH=%ProgramFiles(x86)%\HTML Help Workshop;%PATH%
 @if exist "%VCINSTALLDIR%VCPackages" set PATH=%VCINSTALLDIR%VCPackages;%PATH%
-@set PATH=%FrameworkDir%%Framework35Version%;%PATH%
-@set PATH=%FrameworkDir%%FrameworkVersion%;%PATH%
-@set PATH=%VSINSTALLDIR%Common7\Tools;%PATH%
+@if exist "%FrameworkDir%%Framework40Version%" set PATH=%FrameworkDir%%Framework40Version%;%PATH%
+@if exist "%FrameworkDir%%FrameworkVersion%" set PATH=%FrameworkDir%%FrameworkVersion%;%PATH%
+@if exist "%VSINSTALLDIR%Common7\Tools" set PATH=%VSINSTALLDIR%Common7\Tools;%PATH%
 @if exist "%VCINSTALLDIR%BIN" set PATH=%VCINSTALLDIR%BIN;%PATH%
 @set PATH=%DevEnvDir%;%PATH%
+
+@rem Add path to MSBuild Binaries
+@if exist "%ProgramFiles%\MSBuild\12.0\bin" set PATH=%ProgramFiles%\MSBuild\12.0\bin;%PATH%
+@if exist "%ProgramFiles(x86)%\MSBuild\12.0\bin" set PATH=%ProgramFiles(x86)%\MSBuild\12.0\bin;%PATH%
+
 
 @if exist "%VSINSTALLDIR%VSTSDB\Deploy" (
 	@set "PATH=%VSINSTALLDIR%VSTSDB\Deploy;%PATH%"
@@ -68,14 +74,14 @@
 @rem -------
 @if exist "%VCINSTALLDIR%ATLMFC\LIB" set LIBPATH=%VCINSTALLDIR%ATLMFC\LIB;%LIBPATH%
 @if exist "%VCINSTALLDIR%LIB" set LIBPATH=%VCINSTALLDIR%LIB;%LIBPATH%
-@set LIBPATH=%FrameworkDir%%Framework35Version%;%LIBPATH%
+@if exist "%FrameworkDir%%Framework40Version%" set LIBPATH=%FrameworkDir%%Framework40Version%;%LIBPATH%
 @set LIBPATH=%FrameworkDir%%FrameworkVersion%;%LIBPATH%
 
 @goto end
 
 @REM -----------------------------------------------------------------------
 :GetVSCommonToolsDir
-@set VS110COMNTOOLS=
+@set VS120COMNTOOLS=
 @call :GetVSCommonToolsDirHelper32 HKLM > nul 2>&1
 @if errorlevel 1 call :GetVSCommonToolsDirHelper32 HKCU > nul 2>&1
 @if errorlevel 1 call :GetVSCommonToolsDirHelper64  HKLM > nul 2>&1
@@ -83,32 +89,36 @@
 @exit /B 0
 
 :GetVSCommonToolsDirHelper32
-@for /F "tokens=1,2*" %%i in ('reg query "%1\SOFTWARE\Microsoft\VisualStudio\SxS\VS7" /v "11.0"') DO (
-	@if "%%i"=="11.0" (
-		@SET "VS110COMNTOOLS=%%k"
+@for /F "tokens=1,2*" %%i in ('reg query "%1\SOFTWARE\Microsoft\VisualStudio\SxS\VS7" /v "12.0"') DO (
+	@if "%%i"=="12.0" (
+		@SET "VS120COMNTOOLS=%%k"
 	)
 )
-@if "%VS110COMNTOOLS%"=="" exit /B 1
-@SET "VS110COMNTOOLS=%VS110COMNTOOLS%Common7\Tools\"
+@if "%VS120COMNTOOLS%"=="" exit /B 1
+@SET "VS120COMNTOOLS=%VS120COMNTOOLS%Common7\Tools\"
 @exit /B 0
 
 :GetVSCommonToolsDirHelper64
-@for /F "tokens=1,2*" %%i in ('reg query "%1\SOFTWARE\Wow6432Node\Microsoft\VisualStudio\SxS\VS7" /v "11.0"') DO (
-	@if "%%i"=="11.0" (
-		@SET "VS110COMNTOOLS=%%k"
+@for /F "tokens=1,2*" %%i in ('reg query "%1\SOFTWARE\Wow6432Node\Microsoft\VisualStudio\SxS\VS7" /v "12.0"') DO (
+	@if "%%i"=="12.0" (
+		@SET "VS120COMNTOOLS=%%k"
 	)
 )
-@if "%VS110COMNTOOLS%"=="" exit /B 1
-@SET "VS110COMNTOOLS=%VS110COMNTOOLS%Common7\Tools\"
+@if "%VS120COMNTOOLS%"=="" exit /B 1
+@SET "VS120COMNTOOLS=%VS120COMNTOOLS%Common7\Tools\"
 @exit /B 0
 
 @REM -----------------------------------------------------------------------
-:error_no_VS110COMNTOOLSDIR
+:error_no_VS120COMNTOOLSDIR
 @echo ERROR: Cannot determine the location of the VS Common Tools folder.
 @goto end
 
 :error_no_VSINSTALLDIR
 @echo ERROR: Cannot determine the location of the VS installation.
+@goto end
+
+:error_no_VCINSTALLDIR
+@echo ERROR: Cannot determine the location of the VC installation.
 @goto end
 
 :error_no_FrameworkDIR32
@@ -119,8 +129,9 @@
 @echo ERROR: Cannot determine the version of the .NET Framework 32bit installation.
 @goto end
 
-:error_no_Framework35Version
-@echo ERROR: Cannot determine the .NET Framework 3.5 version.
+:error_no_Framework40Version
+@echo ERROR: Cannot determine the .NET Framework 4.0 version.
 @goto end
 
 :end
+
