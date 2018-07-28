626e9dc99cf1ff6c73f9122555f52bffe89655e2
john <john.t.taylor@gmail.com>
2018-07-28 13:49:05 -0600

    Housekeeping cleanup

--- src/Cpl/Itc/_0test/hw_mvc/WriteSync.cpp
+++ src/Cpl/Itc/_0test/hw_mvc/WriteSync.cpp
@@ -1,13 +1,13 @@
-/*----------------------------------------------------------------------------- 
-* This file is part of the Colony.Core Project.  The Colony.Core Project is an   
-* open source project with a BSD type of licensing agreement.  See the license  
-* agreement (license.txt) in the top/ directory or on the Internet at           
+/*-----------------------------------------------------------------------------
+* This file is part of the Colony.Core Project.  The Colony.Core Project is an
+* open source project with a BSD type of licensing agreement.  See the license
+* agreement (license.txt) in the top/ directory or on the Internet at
 * http://integerfox.com/colony.core/license.txt
-*                                                                               
-* Copyright (c) 2014-2018  John T. Taylor                                        
-*                                                                               
-* Redistributions of the source code must retain the above copyright notice.    
-*----------------------------------------------------------------------------*/ 
+*
+* Copyright (c) 2014-2018  John T. Taylor
+*
+* Redistributions of the source code must retain the above copyright notice.
+*----------------------------------------------------------------------------*/
 /** @file */
 
 #include "Cpl/Itc/_0test/WriteSync.h"
@@ -15,17 +15,17 @@
 
 
 ///////////////////
-WriteSync::WriteSync(Cpl::Itc::PostApi& myMbox)
-:m_mbox(myMbox)
-    {
-    }
+WriteSync::WriteSync( Cpl::Itc::PostApi& myMbox )
+    :m_mbox( myMbox )
+{
+}
 
 
 ///////////////////
 void WriteSync::write( int newValue ) throw()
-    {
-	WritePayload                  payload( newValue );
+{
+    WritePayload                  payload( newValue );
     Cpl::Itc::SyncReturnHandler   srh;
-    WriteMsg 	                  msg(*this,payload,srh);
-    m_mbox.postSync(msg);
-    }
+    WriteMsg 	                  msg( *this, payload, srh );
+    m_mbox.postSync( msg );
+}
--- src/Cpl/Itc/_0test/hw_mvc/mvc.cpp
+++ src/Cpl/Itc/_0test/hw_mvc/mvc.cpp
@@ -1,19 +1,21 @@
-/*----------------------------------------------------------------------------- 
-* This file is part of the Colony.Core Project.  The Colony.Core Project is an   
-* open source project with a BSD type of licensing agreement.  See the license  
-* agreement (license.txt) in the top/ directory or on the Internet at           
+/*-----------------------------------------------------------------------------
+* This file is part of the Colony.Core Project.  The Colony.Core Project is an
+* open source project with a BSD type of licensing agreement.  See the license
+* agreement (license.txt) in the top/ directory or on the Internet at
 * http://integerfox.com/colony.core/license.txt
-*                                                                               
-* Copyright (c) 2014-2018  John T. Taylor                                        
-*                                                                               
-* Redistributions of the source code must retain the above copyright notice.    
-*----------------------------------------------------------------------------*/ 
+*
+* Copyright (c) 2014-2018  John T. Taylor
+*
+* Redistributions of the source code must retain the above copyright notice.
+*----------------------------------------------------------------------------*/
 
 #include "Cpl/Itc/_0test/common.h"
 #include "Bsp/Api.h"
 
 #define REQUIRE(a,b)  if ( !(a) ) Cpl::System::FatalError::logf(b)
 
+#define MY_EVENT_NUMBER     8
+#define MY_EVENT_MASK       0x00000100
 
 /// 
 using namespace Cpl::Itc;
@@ -38,70 +40,71 @@ public:
 public:
     ///
     TestUnit()
-    :m_modelMbox(),
-     m_myModel(m_modelMbox),
-     m_modelViewSAP(m_myModel,m_modelMbox),
-     m_viewerMbox(),
-     m_myViewer( 0, m_viewerMbox, m_modelViewSAP )
-        {
-        }
+        :m_modelMbox( MY_EVENT_MASK ),
+        m_myModel( m_modelMbox ),
+        m_modelViewSAP( m_myModel, m_modelMbox ),
+        m_viewerMbox(),
+        m_myViewer( 0, m_viewerMbox, m_modelViewSAP )
+    {
+    }
 };
 
 
-class Checker: public Cpl::System::Runnable
+class Checker : public Cpl::System::Runnable
 {
 
 public:
     ///
     Checker()
-        {
-        }
+    {
+    }
 
 public:
     ///
     void appRun()
-        {
+    {
         CPL_SYSTEM_TRACE_FUNC( SECT_ );
-       
-        for(;;)
-            {
-            Bsp_Api_turnOn_debug1();    
-            
+
+        for ( ;;)
+        {
+            Bsp_Api_turnOn_debug1();
+
             TestUnit*            testPtr         = new(std::nothrow) TestUnit();
-            Master*              masterRunPtr    = new(std::nothrow) Master( NUM_SEQ_, NUM_WRITES_, testPtr->m_myViewer, testPtr->m_myModel, testPtr->m_myModel, Cpl::System::Thread::getCurrent()); 
+            Master*              masterRunPtr    = new(std::nothrow) Master( NUM_SEQ_, NUM_WRITES_, testPtr->m_myViewer, testPtr->m_myModel, testPtr->m_myModel, Cpl::System::Thread::getCurrent() );
             Cpl::System::Thread* viewerThreadPtr = Cpl::System::Thread::create( testPtr->m_viewerMbox, "Viewer" );
-            Cpl::System::Thread* modelThreadPtr  = Cpl::System::Thread::create( testPtr->m_modelMbox,  "Model" );
+            Cpl::System::Thread* modelThreadPtr  = Cpl::System::Thread::create( testPtr->m_modelMbox, "Model" );
             Cpl::System::Thread* masterThreadPtr = Cpl::System::Thread::create( *masterRunPtr, "MASTER" );
-        
-            CPL_SYSTEM_TRACE_MSG( SECT_, ( "Starting the sequence...." ));
+
+            CPL_SYSTEM_TRACE_MSG( SECT_, ("Starting the sequence....") );
             int i;
-            for(i=0; i< NUM_SEQ_; i++ )
-                {
-                Bsp_Api_toggle_debug2();    
-                testPtr->m_modelMbox.signal();
+            for ( i=0; i < NUM_SEQ_; i++ )
+            {
+                Bsp_Api_toggle_debug2();
+                testPtr->m_modelMbox.notify( MY_EVENT_NUMBER );
                 Cpl::System::Thread::wait();
-                Cpl::System::Api::sleep(50);
+                Cpl::System::Api::sleep( 50 );
                 REQUIRE( testPtr->m_myModel.m_value == (NUM_WRITES_ - 1) * ATOMIC_MODIFY_, "Fail: 1" );
                 REQUIRE( testPtr->m_myViewer.m_attachRspMsg.getPayload().m_value == (NUM_WRITES_ - 1)*ATOMIC_MODIFY_, "Fail: 2" );
                 REQUIRE( testPtr->m_myViewer.m_ownAttachMsg == true, "Fail: 3" );
                 REQUIRE( testPtr->m_myViewer.m_ownDetachMsg == true, "Fail: 4" );
                 REQUIRE( testPtr->m_myViewer.m_pendingCloseMsgPtr == 0, "Fail: 5" );
                 REQUIRE( testPtr->m_myViewer.m_opened == false, "Fail: 6" );
-                REQUIRE( testPtr->m_modelMbox.m_sigCount == i+1, "Fail: 7" );
+                REQUIRE( testPtr->m_modelMbox.m_sigCount == i + 1, "Fail: 7" );
+                REQUIRE( testPtr->m_modelMbox.m_sigCountUnexpected == 0, "Fail: 8" );
                 masterThreadPtr->signal();
-                }
+            }
 
             // Wait for all of the sequences to complete
-            CPL_SYSTEM_TRACE_MSG( SECT_, ( "Sequence Complete: Waiting for all threads to finish" ));
-            Bsp_Api_turnOff_debug2();    
+            CPL_SYSTEM_TRACE_MSG( SECT_, ("Sequence Complete: Waiting for all threads to finish") );
+            Bsp_Api_turnOff_debug2();
             Cpl::System::Thread::wait();
 
             // Shutdown threads
-            CPL_SYSTEM_TRACE_MSG( SECT_, ( "Shutting down threads..." ));
-            Bsp_Api_turnOff_debug1();    
+            CPL_SYSTEM_TRACE_MSG( SECT_, ("Shutting down threads...") );
+            Bsp_Api_turnOff_debug1();
             testPtr->m_viewerMbox.pleaseStop();
             testPtr->m_modelMbox.pleaseStop();
-            Cpl::System::Api::sleep(50); // allow time for threads to stop
+            Cpl::System::Api::sleep( 50 ); // allow time for threads to stop
             REQUIRE( viewerThreadPtr->isRunning() == false, "Fail: 8" );
             REQUIRE( modelThreadPtr->isRunning() == false, "Fail: 9" );
             REQUIRE( masterThreadPtr->isRunning() == false, "Fail: 10" );
@@ -111,16 +114,16 @@ public:
             Cpl::System::Thread::destroy( *masterThreadPtr );
             delete masterRunPtr;
             delete testPtr;
-            
+
             // Test done -->flash LED to indicate successful test (i.e. not stuck in the FatalError handler)
-            CPL_SYSTEM_TRACE_MSG( SECT_, ( "All threads to destroyed!" ));
-            for(i=0; i<4*2; i++)
-                {
-                Bsp_Api_toggle_debug1();    
-                Cpl::System::Api::sleep(250);
-                }
+            CPL_SYSTEM_TRACE_MSG( SECT_, ("All threads to destroyed!") );
+            for ( i=0; i < 4 * 2; i++ )
+            {
+                Bsp_Api_toggle_debug1();
+                Cpl::System::Api::sleep( 250 );
             }
         }
+    }
 };
 
 };  // end namespace
@@ -133,9 +136,9 @@ static Checker checker_;
 
 ////////////////////////////////////////////////////////////////////////////////
 void runtests( void )
-    {
+{
     Cpl::System::Thread::create( checker_, "CHECKER" );
- 
-    // Start the schedular
+
+    // Start the scheduler
     Cpl::System::Api::enableScheduling();
-    }
+}
