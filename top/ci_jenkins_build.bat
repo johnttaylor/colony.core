565682c413a2e359a7fd39af736ad57e8cfa6255
john <john.t.taylor@gmail.com>
2019-04-21 15:57:08 -0600

    working on labeling the build

--- top/ci_jenkins_build.bat
+++ top/ci_jenkins_build.bat
@@ -23,7 +23,7 @@ IF "/%1"=="/" goto :beginbuild
 set TAG=%PKG_BRANCH%-bldnum-%BLDNUM%
 echo:build tag: %TAG% >> build_number_%BLDNUM%.txt
 pushd colony.core
-set HELPER=%TOP_DIR\git-credential-helper.bat
+set HELPER=%TOP_DIR%git-credential-helper.bat
 set GIT_ASKPASS=%HELPER%
 git tag %TAG%
 IF NOT ERRORLEVEL 1 goto :tagready
