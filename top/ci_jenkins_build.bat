239a7501e64475b3707b444c71674ba980f81cb7
john <john.t.taylor@gmail.com>
2019-04-21 15:19:48 -0600

    fix issue with generating the build number

--- top/ci_jenkins_build.bat
+++ top/ci_jenkins_build.bat
@@ -7,7 +7,9 @@
 set PKG_BRANCH=%1
 
 :: Calculate the build number (is the total number of commits acrossed master and develop branches in the Git repo)
+pushd colony.core
 for /f %%i in ('git rev-list --count origin/master') do set MASTER_COUNT=%%i
+popd
 for /f %%i in ('git rev-list --count origin/develop') do set DEVELOP_COUNT=%%i
 set /A BLDNUM=%MASTER_COUNT%+%DEVELOP_COUNT%
 echo:BuildNumber=%BLDNUM% > build_number_%BLDNUM%.txt
