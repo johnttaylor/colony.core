dc4ec3ab28fe87e4933e6a12a503b27962ad1ec7
john <john.t.taylor@gmail.com>
2019-04-28 09:23:11 -0600

    fix build errro

--- tests/Bsp/UserSpace/_0test/windows/vc12/colony_map.h
+++ tests/Bsp/UserSpace/_0test/windows/vc12/colony_map.h
@@ -7,7 +7,7 @@
 
 // Cpl::System mappings
 #ifdef BUILD_VARIANT_WIN32
-#include "Cpl/System/Win32/_win32/mappings_.h"
+#include "Cpl/System/Win32/mappings_.h"
 #endif
 #ifdef BUILD_VARIANT_CPP11
 #include "Cpl/System/Cpp11/mappings_.h"
