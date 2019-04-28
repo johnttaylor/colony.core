49e0287ce05ae53a393d7cfdf74e8e05d7566490
john <john.t.taylor@gmail.com>
2019-04-28 11:41:22 -0600

    update automated unittest projects for faster builds

--- tests/Cpl/Text/Frame/_0test/windows/mingw_w64/mytoolchain.py
+++ tests/Cpl/Text/Frame/_0test/windows/mingw_w64/mytoolchain.py
@@ -22,7 +22,7 @@
 
 # get definition of the Options structure
 from nqbplib.base import BuildValues
-
+from nqbplib.my_globals import NQBP_WORK_ROOT
 
 #===================================================
 # BEGIN EDITS/CUSTOMIZATIONS
@@ -35,20 +35,27 @@ FINAL_OUTPUT_NAME = 'b.exe'
 unit_test_objects = '_BUILT_DIR_.src/Cpl/Text/Frame/_0test'
 
 
+#
+# For build config/variant: "Release"
+#
+
 # Set project specific 'base' (i.e always used) options
 base_release = BuildValues()        # Do NOT comment out this line
-base_release.cflags    = '-m32 -std=c++11 -Wall -Werror -x c++  -fprofile-arcs -ftest-coverage'
+base_release.cflags    = '-m32 -std=c++11 -Wall -Werror -x c++  -fprofile-arcs -ftest-coverage -DCATCH_CONFIG_FAST_COMPILE'
 base_release.linkflags = '-m32 -fprofile-arcs'
 base_release.linklibs  = '-lgcov'
 base_release.firstobjs = unit_test_objects
 
 # Set project specific 'optimized' options
-optimzed_release = BuildValues()    # Do NOT comment out this line
-optimzed_release.cflags = '-O3'
+optimzed_release           = BuildValues()    # Do NOT comment out this line
+optimzed_release.cflags    = '-O3'
+optimzed_release.linklibs  = r'{}\xpkgs\catch\src\Catch\libs\x86\windows\mingw_64\cpp11\32bit\release\library.a'.format( NQBP_WORK_ROOT() )
+optimzed_release.linklibs += ' -lstdc++'
 
 # Set project specific 'debug' options
-debug_release = BuildValues()       # Do NOT comment out this line
-#debug_release.cflags = '-D_MY_APP_DEBUG_SWITCH_'
+debug_release           = BuildValues()       # Do NOT comment out this line
+debug_release.linklibs  = r'{}\xpkgs\catch\src\Catch\libs\x86\windows\mingw_64\cpp11\32bit\debug\library.a'.format( NQBP_WORK_ROOT() )
+debug_release.linklibs += ' -lstdc++'
 
 
 
@@ -64,13 +71,19 @@ optimzed_cpp11 = BuildValues()
 debug_cpp11    = BuildValues()
 
 # Set 'base' options
-base_cpp11.cflags     = '-m64 -std=c++11 -Wall -Werror -x c++  -fprofile-arcs -ftest-coverage'
+base_cpp11.cflags     = '-m64 -std=c++11 -Wall -Werror -x c++  -fprofile-arcs -ftest-coverage -DCATCH_CONFIG_FAST_COMPILE'
 base_cpp11.linkflags  = '-m64 -fprofile-arcs'
 base_cpp11.linklibs   = '-lgcov'
 base_cpp11.firstobjs  = unit_test_objects
 
 # Set 'Optimized' options
-optimzed_cpp11.cflags = '-O3'
+optimzed_cpp11.cflags    = '-O3'
+optimzed_cpp11.linklibs  = r'{}\xpkgs\catch\src\Catch\libs\x86\windows\mingw_64\cpp11\64bit\release\library.a'.format( NQBP_WORK_ROOT() )
+optimzed_cpp11.linklibs += ' -lstdc++'
+
+# Set 'debug' options
+debug_cpp11.linklibs  = r'{}\xpkgs\catch\src\Catch\libs\x86\windows\mingw_64\cpp11\64bit\debug\library.a'.format( NQBP_WORK_ROOT() )
+debug_cpp11.linklibs += ' -lstdc++'
 
 
 #
@@ -85,13 +98,19 @@ optimzed_win64 = BuildValues()
 debug_win64    = BuildValues()
 
 # Set 'base' options
-base_win64.cflags     = '-m64 -std=c++11 -Wall -Werror -x c++  -fprofile-arcs -ftest-coverage'
+base_win64.cflags     = '-m64 -std=c++11 -Wall -Werror -x c++  -fprofile-arcs -ftest-coverage -DCATCH_CONFIG_FAST_COMPILE'
 base_win64.linkflags  = '-m64 -fprofile-arcs'
 base_win64.linklibs   = '-lgcov'
 base_win64.firstobjs  = unit_test_objects
 
 # Set 'Optimized' options
-optimzed_cpp11.cflags = '-O3'
+optimzed_win64.cflags    = '-O3'
+optimzed_win64.linklibs  = r'{}\xpkgs\catch\src\Catch\libs\x86\windows\mingw_64\cpp11\64bit\release\library.a'.format( NQBP_WORK_ROOT() )
+optimzed_win64.linklibs += ' -lstdc++'
+
+# Set 'debug' options
+debug_win64.linklibs  = r'{}\xpkgs\catch\src\Catch\libs\x86\windows\mingw_64\cpp11\64bit\debug\library.a'.format( NQBP_WORK_ROOT() )
+debug_win64.linklibs += ' -lstdc++'
 
 #-------------------------------------------------
 # ONLY edit this section if you are ADDING options
@@ -144,4 +163,3 @@ from nqbplib.toolchains.windows.mingw_w64.console_exe import ToolChain
 def create():
     tc = ToolChain( FINAL_OUTPUT_NAME, prjdir, build_variants, "win64" )
     return tc 
-
