9a3d8332306e6ad2d1df48b9c210c17512dd03c1
john <john.t.taylor@gmail.com>
2020-02-18 19:21:40 -0700

    update copyright statements

--- src/Cpl/Dm/Persistence/Record/Server.cpp
+++ src/Cpl/Dm/Persistence/Record/Server.cpp
@@ -4,7 +4,7 @@
 * agreement (license.txt) in the root directory or on the Internet at
 * http://www.shift-right.com/openrepo/license.htm
 *
-* Copyright (c) 2001-2010 John T. Taylor
+* Copyright (c) 2010-2020  John T. Taylor
 *
 * Redistributions of the source code must retain the above copyright notice.
 *----------------------------------------------------------------------------*/
