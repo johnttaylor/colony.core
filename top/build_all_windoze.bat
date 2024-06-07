4bd4f2dc6f6408fa2bf21b63230ab5bd7b214e40
John Taylor <john.t.taylor@gmail.com>
2024-06-07 11:54:01 -0400

    fix build script

--- top/build_all_windoze.bat
+++ top/build_all_windoze.bat
@@ -12,8 +12,8 @@ set _ROOT=%_TOPDIR%..
 :: Set Build info (and force build number to zero for "non-official" builds)
 set BUILD_TYPE=%2
 set BUILD_NUMBER=%1
-IF %BUILD_TYPE%=="pr" set BUILD_NUMBER=0
-IF %BUILD_TYPE%=="unknown" set BUILD_NUMBER=0
+IF "%BUILD_TYPE%"=="pr" set BUILD_NUMBER=0
+IF "%BUILD_TYPE%"=="unknown" set BUILD_NUMBER=0
 echo:
 echo:BUILD TYPE=%BUILD_TYPE%, BUILD_NUMBER=%BUILD_NUMBER%
 echo:
@@ -25,10 +25,10 @@ mkdir _artifacts
 
 :: Run Doxygen first 
 cd %_TOPDIR%
-::run_doxygen.py %BUILD_TYPE% %BUILD_NUMBER%
-::IF ERRORLEVEL 1 EXIT /b 1
-::copy %_ROOT%\docs\colony.core-library_%BUILD_NUMBER%-%BUILD_TYPE%.chm %_ROOT%\_artifacts\colony.core-library__%BUILD_NUMBER%-%BUILD_TYPE%.chm
-::IF ERRORLEVEL 1 EXIT /b 1
+run_doxygen.py %BUILD_TYPE% %BUILD_NUMBER%
+IF ERRORLEVEL 1 EXIT /b 1
+copy %_ROOT%\docs\colony.core-library_%BUILD_NUMBER%-%BUILD_TYPE%.chm %_ROOT%\_artifacts\colony.core-library__%BUILD_NUMBER%-%BUILD_TYPE%.chm
+IF ERRORLEVEL 1 EXIT /b 1
 
 ::
 :: Build Mingw projects
