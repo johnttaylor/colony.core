80cbfc8395e66dd4f831108948c2c91e2d0b1234
John Taylor <john.t.taylor@gmail.com>
2024-06-06 21:04:07 -0400

    git linux build all script working

--- top/build_linux.sh
+++ top/build_linux.sh
@@ -11,9 +11,9 @@ set -e
 # setup the environment
 source ./env.sh default
 
-# Build all test linux projects 
+# Build all test linux projects (Note: Only building 64bit builds for now) 
 pushd tests
-$NQBP_BIN/other/bob.py -v4 linux  --bld-all posix64 --bldtime --bldnum $1
+$NQBP_BIN/other/bob.py -v4 linux  -b posix64 --bldtime --bldnum $1
 
 # Run unit tests 
 $NQBP_BIN/other/chuck.py -v --match a.out --dir _posix64
