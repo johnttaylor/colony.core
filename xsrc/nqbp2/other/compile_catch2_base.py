2d115326a2143f393a55aa779ec429e1bb78f83f
John Taylor <john.t.taylor@gmail.com>
2024-12-29 14:54:09 -0500

    fix script error

--- xsrc/nqbp2/other/compile_catch2_base.py
+++ xsrc/nqbp2/other/compile_catch2_base.py
@@ -17,13 +17,13 @@
 # <prj1>: <build-type> // e.g. windows\vc12, windows\mingw_w64, linux/gcc, etc.
 #-----------------------------------------------------------------------------
 
+#
+import os, sys, shutil
+
 # Skip when doing a CI build (i.e. relying on the 'build-all' script o have already built the Catch2 library)
 NQBP_CI_BUILD = os.environ.get('NQBP_CI_BUILD')
 if ( NQBP_CI_BUILD != None ):
     sys.exit(0)
-
-#
-import os, sys, shutil
 
 print( "= Catch2 Library..." )
 VERBOSE   = sys.argv[2]
