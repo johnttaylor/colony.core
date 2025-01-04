5bd4584c244efa763d04a0c297b4a81698739cf6
John Taylor <john.t.taylor@gmail.com>
2025-01-04 11:00:08 -0500

    PhonyStdio 'serial driver' so an applicaiton can use littlefs as it CPL file abstraction (because the by default the CPL stream/file abstraction share the same underlying stream/file access)

---
+++ src/Cpl/Io/Serial/PhonyStdio/InputOutput.h
@@ -0,0 +1,80 @@
+#ifndef Cpl_Io_Serial_PhonyStdio_InputOutput_h_
+#define Cpl_Io_Serial_PhonyStdio_InputOutput_h_
+/*-----------------------------------------------------------------------------
+ * This file is part of the Colony.Core Project.  The Colony.Core Project is an
+ * open source project with a BSD type of licensing agreement.  See the license
+ * agreement (license.txt) in the top/ directory or on the Internet at
+ * http://integerfox.com/colony.core/license.txt
+ *
+ * Copyright (c) 2014-2022  John T. Taylor
+ *
+ * Redistributions of the source code must retain the above copyright notice.
+ *----------------------------------------------------------------------------*/
+/** @file */
+
+#include "Cpl/Io/InputOutput.h"
+
+///
+namespace Cpl {
+///
+namespace Io {
+///
+namespace Serial {
+///
+namespace PhonyStdio {
+
+
+/** This concrete class implements the InputOutput stream interface
+    built on top the C Library's fread/fwrite functions with the 'stdin' and
+    'stdout' file handles.
+ */
+class InputOutput : public Cpl::Io::InputOutput
+{
+public:
+    /// Constructor
+    InputOutput();
+
+    /// Destructor
+    ~InputOutput();
+
+
+public:
+    /// Pull in overloaded methods from base class
+    using Cpl::Io::InputOutput::read;
+
+    /** See Cpl::Io::Input.  Note: This method only attempts to read at most
+        one byte from the 'stdin' file handle.  This is the 'workaround' for
+        the fread() function blocking until the requested number of bytes are
+        read (instead of returning after reading the number of bytes available).
+     */
+    bool read( void* buffer, int numBytes, int& bytesRead );
+
+    /// See Cpl::Io::Input. Note: This method always returns true (i.e not supported)
+    bool available();
+
+public:
+    /// Pull in overloaded methods from base class
+    using Cpl::Io::InputOutput::write;
+
+    /// See Cpl::Io::Output
+    bool write( const void* buffer, int maxBytes, int& bytesWritten );
+
+    /// See Cpl::Io::Output
+    void flush();
+
+    /// See Cpl::Io::IsEos.
+    bool isEos();
+
+    /// See Cpl::Io::Output
+    void close();
+
+protected:
+    /// End-of-Stream state
+    bool m_eos;
+};
+
+}  // end namespaces
+}
+}
+}
+#endif  // end header latch
---
+++ src/Cpl/Io/Serial/PhonyStdio/README.txt
@@ -0,0 +1,23 @@
+/** @namespace Cpl::Io::Serial::PhonyStdio
+
+The PhonyStdio namespace provides a crude implementation of the Cpl::Io::InputOutput 
+interface using the C Library's fread/fwrite functions.  This allows the TShell
+and the Trace interfaces to be decoupled-from/not-rely-on the Cpl::Io::Stdio
+implementation. 
+
+The use case for this implementation is:
+    1. Your application (e.g. functional simulator) is running on a Host system 
+       such as Windoze or Linux.
+    2. Your application does NOT use the Host's native filesystem for the 
+       implementing the CPL IO Stream/File abstractions (e.g. using littlefs)
+    3. Your application does 'not care' about the poor/crude performance of
+       PhonyStdio::InputOutput class.
+
+NOTE: On some host platforms when using PhonyStdio and there is blocked call
+      one of the read() methods, the C Library exit() function hangs instead of
+      exiting the application.  On a Linux host - you will need to use the
+      following call to terminate the application
+      
+        #include <signal.h>
+        raise( SIGTERM ); 
+*/  
