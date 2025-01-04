8e34a65d75af9925a0d368314dcb9b362e6640f2
John Taylor <john.t.taylor@gmail.com>
2025-01-04 14:51:15 -0500

    wip

---
+++ src/Cpl/Io/File/Littlefs/BlockDriverApi.h
@@ -0,0 +1,90 @@
+#ifndef Cpl_Io_File_Littlefs_BlockDriverApi_h_
+#define Cpl_Io_File_Littlefs_BlockDriverApi_h_
+/*-----------------------------------------------------------------------------
+ * This file is part of the Colony.Core Project.  The Colony.Core Project is an
+ * open source project with a BSD type of licensing agreement.  See the license
+ * agreement (license.txt) in the top/ directory or on the Internet at
+ * http://integerfox.com/colony.core/license.txt
+ *
+ * Copyright (c) 2014-2020  John T. Taylor
+ *
+ * Redistributions of the source code must retain the above copyright notice.
+ *----------------------------------------------------------------------------*/
+/** @file */
+
+#include "colony_config.h"
+#include "littlefs/lfs.h"
+#include "Driver/SPI/MasterHalfDuplex.h"
+#include "Driver/DIO/Out.h"
+
+
+///
+namespace Cpl {
+///
+namespace Io {
+///
+namespace File {
+///
+namespace Littlefs {
+
+
+/** This partial abstract class defines the interface for block drivers. The 
+    class is also responsible for the actual littlefs callbacks and redirecting
+    the callbacks to a concrete instance of this class.  
+
+    The class - and child classes - are only thread safe when LFS_THREADSAFE
+    is defined.
+ */
+class BlockDriverApi
+{
+public:
+    /** This method is used start/initialize the block driver at run time.  The
+        method MUST be called before it used by littlefs (i.e. BEFORE
+        Cpl::Io::File::Littlefs::initVolume() is called).
+
+        The method returns true if successful; else false is returned when an
+        error occurred.  If false is returned, future  read/prog/erse/sync calls
+        will always return a failure.
+     */
+    virtual bool start() noexcept = 0;
+
+    /** This method is used to stop/shutdown the block driver.  The method MUST be
+        called when the block driver is no longer needed (i.e. AFTER
+        Cpl::Io::File::Littlefs::shutdownVolume() is called).
+     */
+    virtual void stop() noexcept = 0;
+
+
+public:
+    /// littlefs block driver read function (instance based)
+    virtual bool readfn( const struct lfs_config* c, lfs_block_t block, lfs_off_t off, void* buffer, lfs_size_t size ) noexcept = 0;
+
+    /// littlefs block driver prog function (instance based)
+    virtual bool progfn( const struct lfs_config* c, lfs_block_t block, lfs_off_t off, const void* buffer, lfs_size_t size ) noexcept = 0;
+
+    /// littlefs block driver erase function (instance based)
+    virtual bool erasefn( const struct lfs_config* c, lfs_block_t block ) noexcept = 0;
+
+    /// littlefs block driver sync function (instance based)
+    virtual bool syncfn( const struct lfs_config* c ) noexcept = 0;
+
+
+public:
+    /// Use this function when "registering" the block driver's read function
+    static int callback_readfn( const struct lfs_config* c, lfs_block_t block, lfs_off_t off, void* buffer, lfs_size_t size ) noexcept;
+
+    /// Use this function when "registering" the block driver's prog function
+    static int callback_progfn( const struct lfs_config* c, lfs_block_t block, lfs_off_t off, const void* buffer, lfs_size_t size ) noexcept;
+
+    /// Use this function when "registering" the block driver's erase function
+    static int callback_erasefn( const struct lfs_config* c, lfs_block_t block ) noexcept;
+
+    /// Use this function when "registering" the block driver's sync function
+    static int callback_syncfn( const struct lfs_config* c ) noexcept;
+};
+
+}  // end namespace
+}
+}
+}
+#endif  // end header latch
---
+++ src/Cpl/Io/File/Littlefs/_bdspi/BdSpi.cpp
@@ -0,0 +1,275 @@
+/*-----------------------------------------------------------------------------
+ * This file is part of the Colony.Core Project.  The Colony.Core Project is an
+ * open source project with a BSD type of licensing agreement.  See the license
+ * agreement (license.txt) in the top/ directory or on the Internet at
+ * http://integerfox.com/colony.core/license.txt
+ *
+ * Copyright (c) 2014-2020  John T. Taylor
+ *
+ * Redistributions of the source code must retain the above copyright notice.
+ *----------------------------------------------------------------------------*/
+/** @file */
+
+#include "BdSpi.h"
+#include "Cpl/System/Trace.h"
+#include "Cpl/System/Api.h"
+
+using namespace Cpl::Io::File::Littlefs;
+
+#define SECT_            "Cpl::Io::File::Littlefs::BdSpi"
+
+#define WAIT_ON_RESET_MS 2
+
+// FLASH Commands
+enum
+{
+    SFLASH_CMD_READ          = 0x03,
+    SFLASH_CMD_READ_JEDEC_ID = 0x9f,
+    SFLASH_CMD_PAGE_PROGRAM  = 0x02,
+    SFLASH_CMD_READ_STATUS1  = 0x05,
+    SFLASH_CMD_ENABLE_RESET  = 0x66,
+    SFLASH_CMD_RESET         = 0x99,
+    SFLASH_CMD_WRITE_ENABLE  = 0x06,
+    SFLASH_CMD_ERASE_SECTOR  = 0x20,
+};
+
+
+/////////////////////////////////////
+BdSpi::BdSpi( Driver::SPI::MasterHalfDuplex& spi,
+              Driver::DIO::Out&              cs,
+              lfs_size_t                     flashPageSize ) noexcept
+    : m_spi( spi )
+    , m_cs( cs )
+    , m_flashPageSize( flashPageSize )
+    , m_started( false )
+{
+}
+
+BdSpi::~BdSpi() noexcept
+{
+}
+
+////////////////////////////////////////
+bool BdSpi::start() noexcept
+{
+    if ( !m_started )
+    {
+        // Start my supporting drivers
+        if ( !m_spi.start() )
+        {
+            CPL_SYSTEM_TRACE_MSG( SECT_, ( "Failed to start the SPI driver" ) );
+            return false;
+        }
+        if ( !m_cs.start( false ) )
+        {
+            CPL_SYSTEM_TRACE_MSG( SECT_, ( "Failed to start the CS driver" ) );
+            return false;
+        }
+
+        // Read the JEDEC ID to verify the device is present
+        JedecID_T jedecID;
+        if ( readJedecID( jedecID ) )
+        {
+            CPL_SYSTEM_TRACE_MSG( SECT_, ( "JEDEC ID: %02X %02X %02X", jedecID.manufacturerID, jedecID.memoryType, jedecID.capacity ) );
+        }
+        else
+        {
+            CPL_SYSTEM_TRACE_MSG( SECT_, ( "Failed to read the JEDEC ID" ) );
+            return false;
+        }
+
+        // We don't know what state the flash is in so wait for any remaining writes before proceeding
+        if ( !waitUntilReady() )
+        {
+            CPL_SYSTEM_TRACE_MSG( SECT_, ( "Timed out while waiting for the chip to be in the ready state (before soft reset)." ) );
+            return false;
+        }
+
+        // Reset the device
+        if ( !sendCommand( SFLASH_CMD_ENABLE_RESET ) )
+        {
+            CPL_SYSTEM_TRACE_MSG( SECT_, ( "Failed to enable the reset" ) );
+            return false;
+        }
+        if ( !sendCommand( SFLASH_CMD_RESET ) )
+        {
+            CPL_SYSTEM_TRACE_MSG( SECT_, ( "Failed to reset the device" ) );
+            return false;
+        }
+
+        // Wait at least 30us for the reset
+        Cpl::System::Api::sleep( WAIT_ON_RESET_MS );
+        m_started = true;
+    }
+
+    return true;
+}
+
+void BdSpi::stop() noexcept
+{
+    if ( m_started )
+    {
+        m_started = false;
+        m_spi.stop();
+        m_cs.stop();
+    }
+}
+
+///////////////////////////////////////////////
+bool BdSpi::readfn( const struct lfs_config* c, lfs_block_t block, lfs_off_t off, void* buffer, lfs_size_t size ) noexcept
+{
+    // validate that the read is valid
+    if ( block >= c->block_count ||
+         ( off % c->read_size ) != 0 ||
+         ( size % c->read_size ) != 0 ||
+         ( off + size ) > c->block_size )
+    {
+        return false;
+    }
+
+    // Starting address/offset
+    lfs_off_t address = block * c->block_size + off;
+
+    // Read command (24-bit address MSB first) PLUS rx clock cycles
+    uint8_t cmdAndAddr[4] = { SFLASH_CMD_READ,
+                              (uint8_t)( ( address & 0xFF0000 ) >> 16 ),
+                              (uint8_t)( ( address & 0x00FF00 ) >> 8 ),
+                              (uint8_t)( ( address & 0x0000FF ) ) };
+    m_cs.assertOutput();
+    m_spi.transmit( 4, cmdAndAddr );
+    bool status = m_spi.receive( size, buffer );
+    m_cs.deassertOutput();
+
+    return status;
+}
+
+bool BdSpi::progfn( const struct lfs_config* c, lfs_block_t block, lfs_off_t off, const void* buffer, lfs_size_t size ) noexcept
+{
+    // check if the write is valid
+    if ( block >= c->block_count ||
+         ( off % c->prog_size ) != 0 ||
+         ( size % c->prog_size ) != 0 ||
+         ( off + size ) > c->block_size )
+    {
+        return false;
+    }
+
+    // Starting address/offset
+    lfs_off_t address = block * c->block_size + off;
+    auto      srcPtr  = (const uint8_t*)buffer;
+
+    // write one page at a time
+    lfs_size_t remaining = size;
+    while ( remaining )
+    {
+        lfs_size_t const leftOnPage = m_flashPageSize - ( address & ( m_flashPageSize - 1 ) );
+        lfs_size_t const toWrite    = min( remaining, leftOnPage );
+        if ( !writeToPage( address, srcPtr, toWrite ) )
+        {
+            return false;
+        }
+
+        remaining -= toWrite;
+        srcPtr    += toWrite;
+        address   += toWrite;
+    }
+    return true;
+}
+
+bool BdSpi::erasefn( const struct lfs_config* c, lfs_block_t block ) noexcept
+{
+    // Validate the block number
+    if ( block >= c->block_count )
+    {
+        return false;
+    }
+    uint32_t sectorAddress = block * c->block_size;
+
+    // Before we erase the sector we need to wait for any writes to finish
+    waitUntilReady();
+
+    // Enable writing
+    sendCommand( SFLASH_CMD_WRITE_ENABLE );
+
+    // Erase command (24-bit address MSB first)
+    uint8_t cmdAndAddr[4] = { SFLASH_CMD_ERASE_SECTOR,
+                              (uint8_t)( ( sectorAddress & 0xFF0000 ) >> 16 ),
+                              (uint8_t)( ( sectorAddress & 0x00FF00 ) >> 8 ),
+                              (uint8_t)( ( sectorAddress & 0x0000FF ) ) };
+    m_cs.assertOutput();
+    bool status = m_spi.transmit( 4, cmdAndAddr );
+    m_cs.deassertOutput();
+
+    return status;
+}
+
+bool BdSpi::syncfn( const struct lfs_config* c ) noexcept
+{
+    // Ensure all writes are complete
+    return waitUntilReady();
+}
+
+///////////////////////////////////////////////
+bool BdSpi::waitUntilReady() noexcept
+{
+    unsigned retryCount = OPTION_CPL_IO_FILE_LITTLEFS_BDSPI_READ_MAX_RETRIES;
+    while ( retryCount-- && ( readStatus() & 0x01 ) )
+    {
+        Cpl::System::Api::sleep( OPTION_CPL_IO_FILE_LITTLEFS_BDSPI_READ_RETRY_DELAY_MS );
+    }
+
+    return retryCount != 0;
+}
+
+uint8_t BdSpi::readStatus() noexcept
+{
+    uint8_t command = SFLASH_CMD_READ_STATUS1;
+    m_cs.assertOutput();
+    m_spi.transmit( 1, &command );
+    uint8_t status;
+    m_spi.receive( 1, &status );
+    m_cs.deassertOutput();
+
+    return status;
+}
+
+bool BdSpi::sendCommand( uint8_t command ) noexcept
+{
+    m_cs.assertOutput();
+    bool success = m_spi.transmit( 1, &command );
+    m_cs.deassertOutput();
+
+    return success;
+}
+
+bool BdSpi::readJedecID( JedecID_T& dstID ) noexcept
+{
+    uint8_t command = SFLASH_CMD_READ_JEDEC_ID;
+    m_cs.assertOutput();
+    m_spi.transmit( 1, &command );
+    bool success = m_spi.receive( 3, &dstID );
+    m_cs.deassertOutput();
+
+    return success;
+}
+
+bool BdSpi::writeToPage( lfs_off_t offset, const void* buffer, lfs_size_t numBytes ) noexcept
+{
+    // Wait for any previous writes to complete
+    waitUntilReady();
+
+    // Enable writing
+    sendCommand( SFLASH_CMD_WRITE_ENABLE );
+
+    // Page write command (24-bit address MSB first) PLUS data
+    uint8_t cmdAndAddr[4] = { SFLASH_CMD_PAGE_PROGRAM,
+                              (uint8_t)( ( offset & 0xFF0000 ) >> 16 ),
+                              (uint8_t)( ( offset & 0x00FF00 ) >> 8 ),
+                              (uint8_t)( ( offset & 0x0000FF ) ) };
+    m_cs.assertOutput();
+    m_spi.transmit( 4, cmdAndAddr );
+    bool status = m_spi.transmit( numBytes, buffer );
+    m_cs.deassertOutput();
+
+    return status;
+}
