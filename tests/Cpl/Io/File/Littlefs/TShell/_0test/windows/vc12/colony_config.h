ddeb93c490cc9814342cc2b6201d0be88a059129
John Taylor <john.t.taylor@gmail.com>
2025-01-11 14:24:57 -0500

    add missing unit tests

--- tests/Cpl/Io/File/Littlefs/TShell/_0test/windows/vc12/colony_config.h
+++ tests/Cpl/Io/File/Littlefs/TShell/_0test/windows/vc12/colony_config.h
@@ -1,8 +1,7 @@
 #ifndef COLONY_CONFIG_H_
 #define COLONY_CONFIG_H_
 
-// Enable 'TestB'
-#define BUILD_TEST_B_
+#define OPTION_CPL_IO_FILE_LITTLEFS_MAX_VOLUMES 2
 
 // Enable trace
 #define USE_CPL_SYSTEM_TRACE
