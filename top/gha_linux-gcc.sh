d3470ca635bb8fcb80e6d33bd1646188653b85b4
John Taylor <john.t.taylor@gmail.com>
2025-05-24 21:01:10 -0400

    wip

---
+++ top/gha_linux-gcc.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+# This script is used by the CI/Build machine to build the Linux projects
+#
+# The script ASSUMES that the working directory is the package root
+#
+# usage: build_linux.sh <bldnum>
+# 
+
+# setup the environment
+source ./env.sh 
+
+# Set the CI build flag
+export NQBP_CI_BUILD="1"
+
+# Build the Catch2 static library (just 32bit apps for now)
+pushd projects
+$NQBP_BIN/other/bob.py -v4 linux  -c -b posix32 
+popd
+
+# Build all test linux projects (just 32bit apps for now)
+pushd tests
+$NQBP_BIN/other/bob.py -v4 linux  -b posix32 --bldtime --bldnum $1
+
+# Run unit tests
+$NQBP_BIN/other/chuck.py -v --match a.out --dir _posix32
+$NQBP_BIN/other/chuck.py -v --match aa.out --dir _posix32
+$NQBP_BIN/other/chuck.py -v --match a.py --dir _posix32
+$NQBP_BIN/other/chuck.py -v --match aa.py --dir _posix32
+
+
