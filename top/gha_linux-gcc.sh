19cb1e0d0a9c10570a7649796e3d4a430a90e21f
John Taylor <john.t.taylor@gmail.com>
2025-05-24 21:07:43 -0400

    wip

--- top/gha_linux-gcc.sh
+++ top/gha_linux-gcc.sh
@@ -17,17 +17,17 @@ export NQBP_CI_BUILD="1"
 
 # Build the Catch2 static library (just 32bit apps for now)
 pushd projects
-$NQBP_BIN/other/bob.py -v4 linux  -c -b posix32 
+$NQBP_BIN/other/bob.py -v4 linux  -c -b posix 
 popd
 
 # Build all test linux projects (just 32bit apps for now)
 pushd tests
-$NQBP_BIN/other/bob.py -v4 linux  -b posix32 --bldtime --bldnum $1
+$NQBP_BIN/other/bob.py -v4 linux  -b posix --bldtime --bldnum $1
 
 # Run unit tests
-$NQBP_BIN/other/chuck.py -v --match a.out --dir _posix32
-$NQBP_BIN/other/chuck.py -v --match aa.out --dir _posix32
-$NQBP_BIN/other/chuck.py -v --match a.py --dir _posix32
-$NQBP_BIN/other/chuck.py -v --match aa.py --dir _posix32
+$NQBP_BIN/other/chuck.py -v --match a.out --dir _posix
+$NQBP_BIN/other/chuck.py -v --match aa.out --dir _posix
+$NQBP_BIN/other/chuck.py -v --match a.py --dir _posix
+$NQBP_BIN/other/chuck.py -v --match aa.py --dir _posix
 
 
